6d0a2a4264d0a053d67e20d0936335d66bf0da31
ruchika1706 <luthraruchika19@gmail.com>
2020-01-29 12:33:13 +0530

    Minor change

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1 align="left">
+        <h1 align="center">
           Log Dashboard
         </h1>
     </div>
