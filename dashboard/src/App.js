54c5f62f77f17882252fb0bc9d4f710a27707755
ruchika1706 <luthraruchika19@gmail.com>
2020-01-29 12:44:53 +0530

    Minor change

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -8,6 +8,11 @@ function App() {
         <h1 align="center">
           Log Dashboard
         </h1>
+        <form>
+            <input type="radio" name="hour" value="hour"/> 1 hour 
+            <input type="radio" name="day" value="day"/> 1 day
+            <input type="radio" name="week" value="week"/> 1 week 
+        </form>
     </div>
   );
 }
